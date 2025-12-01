b5cdff44fed1cc06cef4b06a8a6114c6a8532985
abdo <Basem@gmail.comabdo>
2025-12-01 22:32:02 +0200

    login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     testImplementation(libs.junit)
--- app/src/main/java/com/example/movieapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/movieapp/ui/screens/LoginScreen.kt
@@ -5,7 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,11 +18,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -29,50 +41,135 @@ import com.example.movieapp.ui.theme.MovieAppTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.FontScaling
 import androidx.compose.ui.unit.sp
 import com.example.movieapp.R
+import com.example.movieapp.ui.theme.Black
+import com.example.movieapp.ui.theme.Blue
+import com.example.movieapp.ui.theme.Darkblue
+import com.example.movieapp.ui.theme.White
 
 @Preview(showBackground = true)
 @Composable
 fun LoginScreen( modifier: Modifier = Modifier){
-
-    // 💡 RECOMMENDATION: Fix the Image modifier chaining here.
-    Column(
+    Box(
         modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 24.dp), // Added horizontal padding for margins
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize()
+            .background(Color(0,9,17))
     )
     {
+        //background image
         Image(
-            painter = painterResource(id = R.drawable.movieta),
-            contentDescription = "logo",
-            modifier = Modifier.size(100.dp) // Removed redundant .fillMaxWidth()
+            painter = painterResource(id = R.drawable.login_background),
+            contentDescription = "Background",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize(),
+            alpha = 0.1f,
         )
-        
-        Spacer(Modifier.height(20.dp))
-        Text(text = "Login With your account !",fontSize = 30.sp)
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
 
-        Spacer(Modifier.height(50.dp))
-
-        var nameInput by remember { mutableStateOf("") }
-        TextField(
-            value = nameInput,
-            onValueChange = { nameInput = it },
-            label = { Text("Username or Email") },
-            modifier = Modifier.fillMaxWidth()
         )
+        {
+            Spacer(Modifier.height(100.dp))
 
-        Spacer(Modifier.height(32.dp))
+            //icon
+            Image(
+                painter = painterResource(id = R.drawable.movieicon),
+                contentDescription = "logo",
+                modifier = Modifier.size(150.dp)
+            )
 
-        var passowrd by remember { mutableStateOf("") }
-        TextField(
-            value = passowrd,
-            onValueChange = { passowrd = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(Modifier.height(32.dp))
-        Button(onClick = {}) { }
+            //welcome
+            Spacer(Modifier.height(20.dp))
+            Text(text = "Welcome back!",fontSize = 30.sp, color = White)
+            Spacer(Modifier.height(10.dp))
+            Text(text = "Please sign in to youre account",fontSize = 15.sp, color = White)
+
+            //username field
+            Spacer(Modifier.height(50.dp))
+            var nameInput by remember { mutableStateOf("") }
+            TextField(
+                value = nameInput,
+                onValueChange = { nameInput = it },
+                label = { Text("Username or Email") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(10.dp),
+                colors = TextFieldDefaults.colors(
+                    unfocusedTextColor = White,
+                    focusedTextColor = White,
+
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+
+                    unfocusedLabelColor = Color.LightGray,
+                    focusedLabelColor = Color.White,
+
+                    unfocusedContainerColor = Darkblue,
+                    focusedContainerColor = Darkblue,
+                )
+            )
+            //password field
+            Spacer(Modifier.height(32.dp))
+            var passowrd by remember { mutableStateOf("") }
+            var passwordVisible by remember { mutableStateOf(false) }
+            TextField(
+                value = passowrd,
+                onValueChange = { passowrd = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(10.dp),
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    val image = if (passwordVisible)
+                        Icons.Filled.Visibility
+                    else
+                        Icons.Filled.VisibilityOff
+
+                    Icon(
+                        imageVector = image,
+                        contentDescription = if (passwordVisible) "Hide password" else "Show password",
+                        modifier = Modifier.clickable() { passwordVisible = !passwordVisible }
+                    )
+                },
+                colors = TextFieldDefaults.colors(
+
+                    unfocusedTextColor = White,
+                    focusedTextColor = White,
+
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+
+                    unfocusedLabelColor = Color.LightGray,
+                    focusedLabelColor = Color.White,
+
+                    unfocusedContainerColor = Darkblue,
+                    focusedContainerColor = Darkblue,
+                )
+            )
+            Spacer(Modifier.height(170.dp))
+            Button(onClick = {}, colors = ButtonDefaults.buttonColors(
+                containerColor = Blue,
+                contentColor = White,
+            ),modifier = Modifier.width(300.dp),) {Text("Sign in")  }
+
+            Row(
+                modifier = modifier.fillMaxWidth().padding(horizontal = 50.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ){Text(text = "Don't have an account ?",fontSize = 15.sp, color = White)
+              TextButton(onClick = { },contentPadding = PaddingValues(0.dp)) {
+                  Text(text = "Sign Up", color = Color.White,fontSize = 15.sp, fontWeight = FontWeight.Bold,)  }
+            }
+
+        }
     }
 }
